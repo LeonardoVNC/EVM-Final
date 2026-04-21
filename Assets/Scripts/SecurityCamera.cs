c1b7eeb782ce64e6b3fda72ba2584db8737da55c
LeonardoVNC <LeonardoVNC@users.noreply.github.com>
2026-04-20 22:35:33 -0400

    fix camera speed control

--- Assets/Scripts/SecurityCamera.cs
+++ Assets/Scripts/SecurityCamera.cs
@@ -8,18 +8,34 @@ public class SecurityCamera : MonoBehaviour
     private float rotationRange = 10f;
     private float rotationSpeed = 0.2f;
 
+    private float localTime = 0f;
+    private bool isActive = false;
+
     void Awake() {
         cam = GetComponent<Camera>();
         initialRotation = transform.localRotation;
+        
+        if (cam != null) cam.enabled = false;
+        this.enabled = false; 
     }
 
     void Update() {
-        float angle = Mathf.Sin(Time.time * rotationSpeed) * rotationRange;
-        transform.localRotation = initialRotation * Quaternion.Euler(0, angle, 0);
+        if (isActive) {
+            localTime += Time.deltaTime * rotationSpeed;
+            
+            float angle = Mathf.Sin(localTime) * rotationRange;
+            transform.localRotation = initialRotation * Quaternion.Euler(0, angle, 0);
+        } else {
+            this.enabled = false;
+        }
     }
 
     public void SetState(bool active) {
+        isActive = active;
         if (cam != null) cam.enabled = active;
-        this.enabled = active;
+        
+        this.enabled = true; 
+        
+        if(active) localTime = 0f; 
     }
 }
