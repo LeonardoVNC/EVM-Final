91224495683944e074da0c86ff4be58bea66db67
LeonardoVNC <LeonardoVNC@users.noreply.github.com>
2026-04-23 08:52:42 -0400

    add battery text UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour {
     public static GameManager Instance { get; private set; }
 
+    public TextMeshProUGUI batteryText; 
+    public Image consumptionImage;
+
     public float batteryLevel = 100f;
     public float baseDrain = 0.1f;
-    public float unitDrain = 0.5f;
+    public float unitDrain = 0.25f;
     public bool hasPower = true;
 
     public bool isSecPanelOn = false;
@@ -22,21 +26,20 @@ public class GameManager : MonoBehaviour {
     void Update() {
         if (hasPower) {
             CalculateBattery();
+            UpdateUI();
         }
     }
 
     void CalculateBattery() {
-        int activeUnits = 0;
-        if (isSecPanelOn) activeUnits+=2;
-        if (isFlashlightOn) activeUnits+=1;
-        if (isDoor1Closed) activeUnits+=1;
-        if (isDoor2Closed) activeUnits+=1;
+        int consumptionUnits = 0;
+        
+        if (isSecPanelOn) consumptionUnits += 2;
+        if (isFlashlightOn) consumptionUnits += 1;
+        if (isDoor1Closed) consumptionUnits += 1;
+        if (isDoor2Closed) consumptionUnits += 1;
 
-        float currentDrain = baseDrain + (activeUnits * unitDrain);
+        float currentDrain = baseDrain + (consumptionUnits * unitDrain);
         batteryLevel -= currentDrain * Time.deltaTime;
-
-        Debug.Log("Bateria:"+batteryLevel);
-        Debug.Log("Consumo:"+currentDrain);
 
         if (batteryLevel <= 0) {
             batteryLevel = 0;
@@ -44,8 +47,25 @@ public class GameManager : MonoBehaviour {
         }
     }
 
+    void UpdateUI() {
+        if (batteryText != null) {
+            batteryText.text = "Batería: " + Mathf.CeilToInt(batteryLevel) + "%";
+        }
+
+        if (consumptionImage != null) {
+            float usageLevel = (isSecPanelOn ? 2 : 0) + (isFlashlightOn ? 1 : 0) +  (isDoor1Closed ? 1 : 0) + (isDoor2Closed ? 1 : 0) + 1;
+
+            consumptionImage.fillAmount = usageLevel / 6f;
+
+            if (usageLevel <= 2) consumptionImage.color = Color.green;
+            else if (usageLevel <= 4) consumptionImage.color = Color.yellow;
+            else consumptionImage.color = Color.red;
+        }
+    }
+
     void PowerOut() {
         hasPower = false;
+        if (batteryText != null) batteryText.text = "Batería: 0%";
         Debug.Log("Todo termino amigos");
     }
 
