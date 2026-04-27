e3f6bea07683b890374fa7b03363db2090359d30
LeonardoVNC <LeonardoVNC@users.noreply.github.com>
2026-04-27 09:29:16 -0400

    add temporal messages and tutorial

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections;
 
 public class UIManager : MonoBehaviour {
     public static UIManager Instance { get; private set; }
@@ -9,6 +10,7 @@ public class UIManager : MonoBehaviour {
     public Image consumptionDisplay;
     public GameObject flashlightIcon;
     public TextMeshProUGUI timeText;
+    public TextMeshProUGUI temporalText;
 
     public GameObject securityUI;
     public GameObject gameUI;
@@ -17,6 +19,11 @@ public class UIManager : MonoBehaviour {
         if (Instance == null) Instance = this;
         else Destroy(gameObject);
     }
+
+    void Start() {
+        StartCoroutine(TutorialSequence());
+    }
+
 
     public void UpdateBatteryUI(float battery, Sprite consumptionSprite) {
         if (batteryText != null) batteryText.text = "Batería: " + Mathf.CeilToInt(battery) + "%";
@@ -34,5 +41,23 @@ public class UIManager : MonoBehaviour {
     public void DisableAllUI() {
         securityUI?.SetActive(false);
         gameUI?.SetActive(false);
+    }
+
+    IEnumerator ShowMessage(string message, float delay) {
+        temporalText.text = message;
+        yield return new WaitForSeconds(delay);
+        temporalText.text = "";
+        yield return new WaitForSeconds(0.5f);
+    }
+
+    IEnumerator TutorialSequence() {
+        if (temporalText == null) yield break;
+
+        yield return ShowMessage("Sobrevive hasta las 6:00 AM", 4f);
+        yield return ShowMessage("Vigila las cámaras para mantenerte a salvo", 4f);
+        yield return ShowMessage("Usa [ESPACIO] para abrir el monitor", 4f);
+        yield return ShowMessage("Usa [CLICK] para encender la linterna", 4f);
+        yield return ShowMessage("Usa [E] y apunta a los botones para cerrar las puertas", 4f);
+        yield return ShowMessage("Buena suerte", 4f);
     }
 }
