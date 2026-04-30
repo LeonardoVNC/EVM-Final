700ec8446d9886c3136ec51061a5ab1d98424c92
LeonardoVNC <LeonardoVNC@users.noreply.github.com>
2026-04-30 15:11:58 -0400

    add flashlight tick behavior on low battery

--- Assets/Scripts/Flashlight.cs
+++ Assets/Scripts/Flashlight.cs
@@ -4,22 +4,61 @@ public class Flashlight : MonoBehaviour {
     private Light lightComponent;
     private bool isOn = false;
 
+    public float thresholdStartTicker = 15f; 
+    private float tickerTimer = 0f;
+    private float tickerSpeed = 0.07f;
+
     void Awake() {
         lightComponent = GetComponent<Light>();
         lightComponent.enabled = false;
     }
 
+    void Update() {
+        if (isOn) {
+            HandleTicker();
+        }
+    }
+
+    void HandleTicker() {
+        float currentBattery = GameManager.Instance.batteryLevel;
+
+        if (currentBattery > thresholdStartTicker) {
+            if (!lightComponent.enabled) SetLightState(true);
+            return;
+        }
+
+        if (currentBattery <= 0) {
+            ForceOff();
+            return;
+        }
+
+        tickerTimer += Time.deltaTime;
+        if (tickerTimer >= tickerSpeed) {
+            tickerTimer = 0;
+
+            float failureChance = 1f - (currentBattery / thresholdStartTicker);
+            bool shouldBeOn = Random.value > failureChance;
+            
+            if (lightComponent.enabled != shouldBeOn) {
+                SetLightState(shouldBeOn);
+            }
+        }
+    }
+
+    private void SetLightState(bool active) {
+        lightComponent.enabled = active;
+        GameManager.Instance.SetFlashlightStatus(active);
+    }
+
     public void Toggle() {
-        if (!GameManager.Instance.hasPower) return;
+        if (GameManager.Instance.batteryLevel <= 0) return;
 
         isOn = !isOn;
-        lightComponent.enabled = isOn;
-        GameManager.Instance.SetFlashlightStatus(isOn);
+        SetLightState(isOn);
     }
 
     public void ForceOff() {
         isOn = false;
-        lightComponent.enabled = false;
-        GameManager.Instance.SetFlashlightStatus(false);
+        SetLightState(false);
     }
 }
