e3a4ae2ef806b62a4fc1b2489a632538ad67ba3e
LeonardoVNC <LeonardoVNC@users.noreply.github.com>
2026-05-06 19:45:15 -0400

    add difficulty management on main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,25 +1,52 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
     private bool isStarting = false;
+    private static int difficulty = 1;
+
     public AudioClip clickClip;
+    public GameObject firstScreen;
+    public GameObject secondScreen;
 
     void Start() {
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+        showFirst(true);
     }
 
-    public void play() {
-        if (isStarting) return;
-
-        isStarting = true;
-        GlobalAudioManager.Instance.PlayGlobalSound(clickClip);
-        SceneManager.LoadScene("Night1");
+    // 1st Screen
+    public void startPlay() {
+        showFirst(false);
+        clickSound();
     }
 
     public void quit() {
-        GlobalAudioManager.Instance.PlayGlobalSound(clickClip);
+        clickSound();
         Application.Quit();
+    }
+
+    // 2nd Screen
+    public void play(int level) {
+        if (isStarting) return;
+
+        isStarting = true;
+        clickSound();
+        difficulty = level;
+        SceneManager.LoadScene("Night1");
+    }
+    
+    public void goBack() {
+        if (isStarting) return;
+        showFirst(true);
+        clickSound();
+    }
+
+    // Otros
+    public void clickSound() => GlobalAudioManager.Instance.PlayGlobalSound(clickClip);
+    public void showFirst(bool show) {
+        firstScreen?.SetActive(show);
+        secondScreen?.SetActive(!show);
     }
 }
